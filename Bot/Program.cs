481e07bcc3371271e1d75db6ecbb11e9c051b10f
Robson Kinoshita <robsonyukiu@gmail.com>
2020-06-06 01:48:09 -0300

    Exemplo de como encaminhar mensagens de um canal para um groupo ou contato

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,38 +1,53 @@
 ﻿using System;
 using Telegram.Bot;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace Bot
+
+namespace bot
 {
     class Program
     {
 
-        private static TelegramBotClient botClient = new TelegramBotClient("1162273722:AAEujxDDurLW-mMBZnlJOm0pKBd8izqenqQ");
-
+        static TelegramBotClient botClient = new TelegramBotClient("SEU API TOKEN AQUI");
         static void Main(string[] args)
         {
             botClient.OnMessage += BotClient_OnMessage;
-
+            botClient.OnUpdate += BotClient_OnUpdate;
             botClient.StartReceiving();
-            Thread.Sleep(Timeout.Infinite);
-            botClient.StopReceiving();
+            Console.ReadLine();
+        }
+
+        private static void BotClient_OnUpdate(object sender, Telegram.Bot.Args.UpdateEventArgs e)
+        {
+            if (e.Update.ChannelPost != null)
+            {
+                Console.WriteLine("Mensagem recebida no canal: " + e.Update.ChannelPost.Text);
+
+                var channelChatId = e.Update.ChannelPost.Chat.Id;
+                var messageId = e.Update.ChannelPost.MessageId;
+
+                //Substituir o -00000 pelo id do chat que vc deseja encaminhar a mensagem
+                botClient.ForwardMessageAsync("-00000000", channelChatId, messageId);
+                                                              
+            }
         }
 
         private static void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            Console.WriteLine(e.Message.Text);
-
-            if(e.Message.Text.ToUpper()=="OI")
+            if (e.Message.Text != null)
             {
-                botClient.SendTextMessageAsync(
-                    e.Message.Chat.Id,
-                    $"Olá {e.Message.From.FirstName}, tudo bem! Seu ID é {e.Message.From.Id} "
-                    );
+                Console.WriteLine(e.Message.Text);
+                Console.WriteLine("O ID do chat é:" + e.Message.Chat.Id);
 
+                if (e.Message.Text.ToUpper() == "OI")
+                {
+                    botClient.SendTextMessageAsync(e.Message.Chat.Id, "E ai, blz? O seu ChatID é " + e.Message.Chat.Id);
+                }
             }
-
-
+            
         }
     }
 }
+
+
+
+
