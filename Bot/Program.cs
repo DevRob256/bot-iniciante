fad5156e18338b4ecfa1678249a4b24d62f03526
Robson Kinoshita <robsonyukiu@gmail.com>
2020-08-02 01:07:08 -0300

    Criação de um exemplo de menu.

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -2,17 +2,19 @@
 using Telegram.Bot;
 using System.Threading;
 using System.Threading.Tasks;
-using Telegram.Bot.Types.Enums;
+
 
 namespace Bot
 {
     class Program
     {
-
-        private static TelegramBotClient botClient = new TelegramBotClient("SEU TOKEN AQUI");
+        private static TelegramBotClient botClient = new TelegramBotClient("1136676393:AAETSwhEcRW9TX39jpiAENOYxkgCyVYKGvQ");
 
         static void Main(string[] args)
         {
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"ID: {me.Id} \nNome do Bot: {me.FirstName} \n_"); //Exibe os resultados de login do bot
+
             botClient.OnMessage += BotClient_OnMessage;
 
             botClient.StartReceiving();
@@ -22,17 +24,65 @@ namespace Bot
 
         private static void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            Console.WriteLine(e.Message.Text);
+            Console.WriteLine($"Mensagem recebida:{e.Message.Text} - de {e.Message.Chat.FirstName}");
 
-            if(e.Message.Text.ToUpper()=="OI")
+            if (e.Message.Text.ToUpper() == "OI")
             {
-                botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat.Id,
-                    text: $"<a href =\"http://www.google.com.br\">Clique aqui</a>",
-                    parseMode: ParseMode.Html
-                    );
+
             }
+
+
+            string opcaoUsuario = e.Message.Text;
+
+            switch (opcaoUsuario)
+                {
+                    case "1":
+                        botClient.SendTextMessageAsync(
+                            e.Message.Chat.Id,$"Olá! {e.Message.From.FirstName}, Você escolheu a opção Parcele Fácil");
+                        break;
+                    case "2":
+                        botClient.SendTextMessageAsync(
+                            e.Message.Chat.Id, $"Olá! {e.Message.From.FirstName}, Você escolheu a opção Crédito Pessoal");
+                    break;
+                    case "3":
+                        botClient.SendTextMessageAsync(
+                            e.Message.Chat.Id, $"Olá! {e.Message.From.FirstName}, Você escolheu a opção Pag Contas");
+                    break;
+                    case "4":
+                        botClient.SendTextMessageAsync(
+                            e.Message.Chat.Id, $"Olá! {e.Message.From.FirstName}, Você escolheu a opção Saque rápido");
+                    break;
+                    case "5":
+                        botClient.SendTextMessageAsync(
+                            e.Message.Chat.Id, $"Olá! {e.Message.From.FirstName}, Você escolheu a opção SMS Controle Total");
+                    break;
+                    case "6":
+                        botClient.SendTextMessageAsync(
+                            e.Message.Chat.Id, $"Olá! {e.Message.From.FirstName}, Você escolheu a opção Extravio do cartão");
+                    break;
+                    case "X":
+                        botClient.SendTextMessageAsync(
+                            e.Message.Chat.Id, $"Olá! {e.Message.From.FirstName}, Você escolheu Sair");
+                    break;
+                    default:
+                        botClient.SendTextMessageAsync(
+                        e.Message.Chat.Id,
+                        $"Olá! {e.Message.From.FirstName}, Seja bem vindo ao atendimento virtual do Banco Carrefour!\n" +
+                        $"Informe a opção desejada:\n" +
+                        $"1- Parcele Fácil\n" +
+                        $"2- Crédito Pessoal\n" +
+                        $"3- Pag Contas\n" +
+                        $"4- Saque rápido\n" +
+                        $"5- SMS Controle Total\n" +
+                        $"6- Extravio do cartão\n" +
+                        $"X- Sair\n");
+                    break;
+
+            }
+
+  
 
         }
     }
 }
+
